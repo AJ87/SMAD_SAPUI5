ae44f79cbd1379bc90ac51b7764cc9469ce63f04
EC2 Default User <ec2-user@ip-172-31-32-134.ap-southeast-2.compute.internal>
2016-10-09 21:02:29 +0000

    Updates

--- webapp/server.js
+++ webapp/server.js
@@ -60,7 +60,7 @@ http.createServer(function (request, response) {
       }
     } else if (filePath == './numberOfChildren') {
       var numberOfChildren = regoFunction.getNumberOfChildren();
-      if (numberOfChildren < 135) {
+      if (numberOfChildren < 130) {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end();
       } else {
@@ -125,4 +125,4 @@ http.createServer(function (request, response) {
     }
 
 }).listen(80);
-console.log('Server running at http://127.0.0.1:8125/');
+console.log('Server running at http://127.0.0.1:80/');
