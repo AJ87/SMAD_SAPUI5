ee9b258f7d080a5f0a13bac50e2938c764b8be2b
AJ87 <ultimateaj87@gmail.com>
2017-11-08 13:12:48 +0000

    Email scripts

--- webapp/server.js
+++ webapp/server.js
@@ -312,52 +312,64 @@ http.createServer(function (request, response) {
       );
     } else {
 
-      console.log(filePath);
+// protect some files from being read
+// don't block robots.txt or manifest.json
+      if ( filePath === './start_server.sh' ||
+           filePath === './package.json' ||
+           filePath === './createemail.sh' ||
+           filePath === './sendemail.sh') {
+            console.log("filepath: " + filePath);
+            response.writeHead(503);
+            response.end('Unauthorised Access');
+            response.end();
+      } else {
 
-      var extname = path.extname(filePath);
-      var contentType = 'text/html';
-      switch (extname) {
-          case '.js':
-              contentType = 'text/javascript';
-              break;
-          case '.css':
-              contentType = 'text/css';
-              break;
-          case '.json':
-              contentType = 'application/json';
-              break;
-          case '.png':
-              contentType = 'image/png';
-              break;
-          case '.jpg':
-              contentType = 'image/jpg';
-              break;
-          case '.wav':
-              contentType = 'audio/wav';
-              break;
+        console.log(filePath);
+
+        var extname = path.extname(filePath);
+        var contentType = 'text/html';
+        switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.wav':
+                contentType = 'audio/wav';
+                break;
+        }
+
+        fs.readFile(filePath, function(error, content) {
+            if (error) {
+                if(error.code == 'ENOENT'){
+                    fs.readFile('./404.html', function(error, content) {
+                        response.writeHead(200, { 'Content-Type': contentType });
+                        response.end(content, 'utf-8');
+                    });
+                }
+                else {
+                    console.log("filepath: " + filePath);
+                    response.writeHead(500);
+                    response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                    response.end();
+                }
+            }
+            else {
+                response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+            }
+        });
       }
-
-      fs.readFile(filePath, function(error, content) {
-          if (error) {
-              if(error.code == 'ENOENT'){
-                  fs.readFile('./404.html', function(error, content) {
-                      response.writeHead(200, { 'Content-Type': contentType });
-                      response.end(content, 'utf-8');
-                  });
-              }
-              else {
-                  console.log("filepath: " + filePath);
-                  response.writeHead(500);
-                  response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                  response.end();
-              }
-          }
-          else {
-              response.writeHead(200, { 'Content-Type': contentType });
-              response.end(content, 'utf-8');
-          }
-      });
-
     }
 
 }).listen(80); //3125 loally and 80 on aws
