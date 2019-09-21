76c9662b302c0e89dfd9345a38e2be18d111b1aa
AJ87 <ultimateaj87@gmail.com>
2019-09-21 14:24:14 +1000

    Remove pre rego email sign up

--- webapp/view/PreRegoFragment.fragment.js
+++ webapp/view/PreRegoFragment.fragment.js
@@ -11,40 +11,40 @@ sap.ui.jsfragment("SMADJS.view.PreRegoFragment", {
 			content:[oText1]
 		});
 
-		var oText2 = new sap.m.Text({
-			text:"If you would like an email reminder when registration is opening" +
-			" submit your email address."
-		});
-
-		var oPanel2 = new sap.m.Panel({
-			class:"SapUiResponsiveMargin",
-			width:"auto",
-			content:[oText2]
-		});
-
-		// pre-rego form
-		oLabel = new sap.m.Label({
-			text:"Email Address",
-			type:"email",
-			required:true
-		});
-		oInput = new sap.m.Input(this.createId("InputPreRegoEmail"),{
-			value:"{/preRego/email}",
-		});
-		var oSimpleForm = new sap.ui.layout.form.SimpleForm();
-
-		oSimpleForm.addContent(oLabel);
-		oSimpleForm.addContent(oInput);
-
-		var oButtonSubmitEmail = new sap.m.Button({
-			text:"Submit",
-			type:"Accept",
-			press:[oController.handleSubmitEmail,oController]
-		});
-
-		var oBar = new sap.m.Bar({
-			contentRight:[oButtonSubmitEmail]
-		});
+		// var oText2 = new sap.m.Text({
+		// 	text:"If you would like an email reminder when registration is opening" +
+		// 	" submit your email address."
+		// });
+		//
+		// var oPanel2 = new sap.m.Panel({
+		// 	class:"SapUiResponsiveMargin",
+		// 	width:"auto",
+		// 	content:[oText2]
+		// });
+		//
+		// // pre-rego form
+		// oLabel = new sap.m.Label({
+		// 	text:"Email Address",
+		// 	type:"email",
+		// 	required:true
+		// });
+		// oInput = new sap.m.Input(this.createId("InputPreRegoEmail"),{
+		// 	value:"{/preRego/email}",
+		// });
+		// var oSimpleForm = new sap.ui.layout.form.SimpleForm();
+		//
+		// oSimpleForm.addContent(oLabel);
+		// oSimpleForm.addContent(oInput);
+		//
+		// var oButtonSubmitEmail = new sap.m.Button({
+		// 	text:"Submit",
+		// 	type:"Accept",
+		// 	press:[oController.handleSubmitEmail,oController]
+		// });
+		//
+		// var oBar = new sap.m.Bar({
+		// 	contentRight:[oButtonSubmitEmail]
+		// });
 
 		var oPreRegoPage = new sap.m.Page({
 			title:"{i18n>appTitle}",
